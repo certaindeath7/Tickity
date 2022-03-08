e42a3fd85cc839bfa4c9a80dfccf7a49958182aa
Duc Nguyen <ducnguyentranphuc98@gmail.com>
2022-03-08 21:22:13 +1100

    Update dependencies and finish Expiration service

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,4 +1,6 @@
 import Queue from 'bull';
+import { natsWrapper } from '../nats-wrapper';
+import { ExpirationCompletePublisher } from '../events/publishers/expiration-complete-publisher';
 
 interface Payload {
   orderId: string;
@@ -11,7 +13,9 @@ const expirationQueue = new Queue<Payload>('order:expiration', {
 
 // cosumer job
 expirationQueue.process(async (job) => {
-  console.log('hello', job.data.orderId);
+  new ExpirationCompletePublisher(natsWrapper.client).publish({
+    orderId: job.data.orderId,
+  });
 });
 
 export default expirationQueue;
